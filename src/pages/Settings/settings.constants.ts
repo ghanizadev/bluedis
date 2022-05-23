ae7c8de52a175ea476e2184d014da0e18e4c8a02
ghanizadev <jf.melo6@gmail.com>
2022-05-23 17:09:09 +0300

    type fix

--- src/pages/Settings/settings.constants.ts
+++ src/pages/Settings/settings.constants.ts
@@ -4,5 +4,5 @@ export const FONTS = ["Roboto", "JetBrains Mono", "Montserrat", "Open Sans"];
 
 export const LANGUAGES = [
   { label: "English", value: Language.en_US },
-  { label: "Portugues", value: Language.pt_BR },
+  { label: "Português", value: Language.pt_BR },
 ];
